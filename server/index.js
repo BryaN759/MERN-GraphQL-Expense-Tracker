c6812ad2cc780416e03259f9e47a3a093cb64535
BryaN759 <khaledmushahed@gmail.com>
2024-08-01 21:21:36 +0600

    deployment setup

--- server/index.js
+++ server/index.js
@@ -1,26 +1,28 @@
-import { ApolloServer } from '@apollo/server';
-
-import { expressMiddleware } from '@apollo/server/express4';
-import { ApolloServerPluginDrainHttpServer } from '@apollo/server/plugin/drainHttpServer';
-import cors from 'cors';
-import dotenv from 'dotenv';
 import express from 'express';
 import http from 'http';
-import { connectDB } from './db/connectDB.js';
+import cors from 'cors';
+import dotenv from 'dotenv';
+import path from 'path';
 import passport from 'passport';
 import session from 'express-session';
 import connectMongo from 'connect-mongodb-session';
 
+import { ApolloServer } from '@apollo/server';
+import { expressMiddleware } from '@apollo/server/express4';
+import { ApolloServerPluginDrainHttpServer } from '@apollo/server/plugin/drainHttpServer';
+
+import { buildContext } from 'graphql-passport';
+
 import mergedResolvers from './resolvers/index.js';
 import mergedTypeDefs from './typeDefs/index.js';
-import { error } from 'console';
-import { buildContext } from 'graphql-passport';
+
+import { connectDB } from './db/connectDB.js';
 import { configurePassport } from './passport/passport.config.js';
 
 dotenv.config();
-
 configurePassport();
 
+const __dirname = path.resolve();
 const app = express();
 
 const httpServer = http.createServer(app);
@@ -29,7 +31,7 @@ const MongoDBStore = connectMongo(session);
 
 const store = new MongoDBStore({
     uri: process.env.MONGO_URI,
-    collection: 'session'
+    collection: 'sessions'
 });
 
 store.on('error', (err) => console.log(err));
@@ -37,11 +39,11 @@ store.on('error', (err) => console.log(err));
 app.use(
     session({
         secret: process.env.SESSION_SECRET,
-        resave: false,
-        saveUninitialized: false,
+        resave: false, // this option specifies whether to save the session to the store on every request
+        saveUninitialized: false, // option specifies whether to save uninitialized sessions
         cookie: {
             maxAge: 1000 * 60 * 60 * 24 * 7,
-            httpOnly: true
+            httpOnly: true // this option prevents the Cross-Site Scripting (XSS) attacks
         },
         store: store
     })
@@ -56,8 +58,11 @@ const server = new ApolloServer({
     plugins: [ApolloServerPluginDrainHttpServer({ httpServer })]
 });
 
+// Ensure we wait for our server to start
 await server.start();
 
+// Set up our Express middleware to handle CORS, body parsing,
+// and our expressMiddleware function.
 app.use(
     '/graphql',
     cors({
@@ -72,9 +77,15 @@ app.use(
     })
 );
 
+// npm run build will build your frontend app, and it will the optimized version of your app
+app.use(express.static(path.join(__dirname, 'client/dist')));
+
+app.get('*', (req, res) => {
+    res.sendFile(path.join(__dirname, 'client/dist', 'index.html'));
+});
+
 // Modified server startup
 await new Promise((resolve) => httpServer.listen({ port: 4000 }, resolve));
-
 await connectDB();
 
 console.log(`🚀 Server ready at http://localhost:4000/graphql`);
